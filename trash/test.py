f77b2d8c5c43bd8a1ff1e98fc8cb0e03edabd24a
Iaroslav Baramykov <yrslvb@yandex.ru>
2023-11-09 20:32:06 +0700

    Исправления в main.yml, docker-compose.production.yml, nginx.conf

---
+++ trash/test.py
@@ -0,0 +1,27 @@
+server {
+    server_name foodkilogram.hopto.org;
+
+    location / {
+    proxy_pass http://127.0.0.1:7000;
+    proxy_set_header        Host $host;
+    }
+
+
+    listen 443 ssl; # managed by Certbot
+    ssl_certificate /etc/letsencrypt/live/foodkilogram.hopto.org/fullchain.pem; # managed by Certbot
+    ssl_certificate_key /etc/letsencrypt/live/foodkilogram.hopto.org/privkey.pem; # managed by Certbot
+    include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+
+}
+
+server {
+    if ($host = foodkilogram.hopto.org) {
+        return 301 https://$host$request_uri;
+    } # managed by Certbot
+
+    server_name foodkilogram.hopto.org;
+    server_tokens off;
+    listen 80;
+    return 404; # managed by Certbot
+}
