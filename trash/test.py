5e4438908321821a077236265a138a07a003dd4d
Iaroslav Baramykov <yrslvb@yandex.ru>
2023-11-11 13:19:26 +0700

    Изменил структуру, docker-compose.production.yml, nginx.conf

--- trash/test.py
+++ trash/test.py
@@ -56,3 +56,57 @@ server {
 
 
 }
+
+
+
+----------------------------------------------------------------------------------------
+# build_and_push_in_to_docker_hub:
+  #   name: Push Docker image to DockerHub
+  #   runs-on: ubuntu-latest
+  #   needs: tests
+  #   steps:
+  #     - name: Check out the repo
+  #       uses: actions/checkout@v3
+
+  #     - name: Set up Docker Buildx
+  #       uses: docker/setup-buildx-action@v2
+
+  #     - name: Login to Docker 
+  #       uses: docker/login-action@v2
+  #       with:
+  #         username: ${{ secrets.DOCKER_USERNAME }} 
+  #         password: ${{ secrets.DOCKER_PASSWORD }}
+
+  #     - name: Push backend to DockerHub
+  #       uses: docker/build-push-action@v4
+  #       with:
+  #         context: ./backend/
+  #         push: true
+  #         tags: ${{ secrets.DOCKER_USERNAME }}/foodgram_backend:latest
+
+  #     - name: Push frontend to DockerHub
+  #       uses: docker/build-push-action@v4
+  #       with:
+  #         context: ./frontend/
+  #         push: true
+  #         tags: ${{ secrets.DOCKER_USERNAME }}/foodgram_frontend:latest
+
+  build_infra_and_push_to_docker_hub:
+    name: Push infra Docker image to DockerHub
+    runs-on: ubuntu-latest
+    steps:
+      - name: Check out the repo
+        uses: actions/checkout@v3
+      - name: Set up Docker Buildx
+        uses: docker/setup-buildx-action@v2
+      - name: Login to Docker 
+        uses: docker/login-action@v2
+        with:
+          username: ${{ secrets.DOCKER_USERNAME }}
+          password: ${{ secrets.DOCKER_PASSWORD }}
+      - name: Push to DockerHub
+        uses: docker/build-push-action@v4
+        with:
+          context: ./infra/
+          push: true
+          tags: mralmostfreeman/foodgram_infra:latest
