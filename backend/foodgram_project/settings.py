8d24faaa3c4f9f70828a4071e447b696112d3fe6
Iaroslav Baramykov <yrslvb@yandex.ru>
2023-11-15 17:20:42 +0700

    Обновил .енв на сервере

--- backend/foodgram_project/settings.py
+++ backend/foodgram_project/settings.py
@@ -120,7 +120,7 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 6,
+    'PAGE_SIZE': os.getenv('PAGE_SIZE'),
 }
 
 DJOSER = {
